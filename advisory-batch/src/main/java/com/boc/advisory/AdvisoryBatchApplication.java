3034f78603476587153c123d5cdc2f4fbe25f93b
DS-FTW <birtholding@163.com>
2025-04-28 22:27:31 +0800

    initiate project

---
+++ advisory-batch/src/main/java/com/boc/advisory/AdvisoryBatchApplication.java
@@ -0,0 +1,11 @@
+package com.boc.advisory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvisoryBatchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdvisoryBatchApplication.class, args);
+    }
+}
---
+++ advisory-batch/src/main/java/com/boc/advisory/entity/UserInfo.java
@@ -0,0 +1,52 @@
+package com.boc.advisory.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_info")
+@Data
+public class UserInfo implements Serializable {
+
+    private static final long serialVersionUID = -6513547984710108491L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "gender")
+    private String gender;
+
+    @Column(name = "contact")
+    private String contactNo;
+
+    @Column(name = "country")
+    private String country;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "created_on")
+    private LocalDateTime createdOn;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    @Column(name = "updated_on")
+    private LocalDateTime updatedOn;
+
+
+}
